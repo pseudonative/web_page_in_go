71a7059deb68e4b4d35b2bb7b8d48db613a54dce
Jeremy Coando <killerpit02@yahoo.com>
2024-01-26 11:00:55 -0700

    added bootstrap

--- main.go
+++ main.go
@@ -2,38 +2,12 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
 const portNumber = ":8080"
-
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
-	if err != nil {
-		fmt.Println("error parsing template: ", err)
-		http.Error(w, "Internal Server Error one", http.StatusInternalServerError)
-		return
-	}
-	err = parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error executing template:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	}
-
-}
 
 // main is the main application function
 func main() {
