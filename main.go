738760367701535105aaa3505ad5436bef5c7eba
Jeremy Coando <killerpit02@yahoo.com>
2024-01-25 20:01:12 -0700

    refactored dirs

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 )
 
 const portNumber = ":8080"
@@ -15,22 +17,34 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-
+	renderTemplate(w, "about.page.tmpl")
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/home.page.tmpl")
-	err := parsedTemplate.Execute(w, nil)
+	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
 	if err != nil {
 		fmt.Println("error parsing template: ", err)
+		http.Error(w, "Internal Server Error one", http.StatusInternalServerError)
 		return
 	}
+	err = parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error executing template:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
 }
 
 // main is the main application function
 func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Println("error getting working directory: ", err)
+	}
+	fmt.Println("Current working directory: ", wd)
+
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	fmt.Println(fmt.Sprintf("starting application on port %s", portNumber))
+	fmt.Printf("starting application on port %s", portNumber)
 	http.ListenAndServe(portNumber, nil)
 }
