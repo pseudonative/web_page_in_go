09f2420e6e6ab769e748b9679854b580b0d9b60e
Beau Coando <killerpit02@yahoo.com>
2024-01-26 18:15:51 -0700

    added middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,15 +3,19 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/pseudonative/web_page_in_go/pkg/config"
 	"github.com/pseudonative/web_page_in_go/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
 
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
